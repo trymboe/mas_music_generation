94b0c0401f1dc4bd407b26e89eda2b6bdd57e8bc
trymboe <trym.boe@gmail.com>
2023-09-12 12:56:51 +0200

    :wrench: chore: add autoformating and config

---
+++ config/hyperparameters_bass.py
@@ -0,0 +1,8 @@
+SEQUENCE_LENGTH_BASS = 10
+VOCAB_SIZE_BASS = 12
+EMBED_SIZE_BASS = 128  # Embedding size
+NHEAD_BASS = 2  # Number of self-attention heads
+NUM_LAYERS_BASS = 2  # Number of transformer layers
+NUM_EPOCHS_BASS = 10
+BATCH_SIZE_BASS = 16
+LEARNING_RATE_BASS = 0.001
