0968a9d79e31434a860feaf66ddedee2e83c734a
trymboe <trym.boe@gmail.com>
2023-10-08 12:53:03 +0200

    :truck: chore: rename config files

--- config/hyperparameters_bass.py
+++
@@ -1,9 +0,0 @@
-SEQUENCE_LENGTH_BASS = 8
-NOTE_VOCAB_SIZE_BASS = 12
-DURATION_VOCAB_SIZE_BASS = 9
-EMBED_SIZE_BASS = 64  # Embedding size
-NHEAD_BASS = 2  # Number of self-attention heads
-NUM_LAYERS_BASS = 2  # Number of transformer layers
-NUM_EPOCHS_BASS = 2  # Number of epochs
-BATCH_SIZE_BASS = 8
-LEARNING_RATE_BASS = 0.001
